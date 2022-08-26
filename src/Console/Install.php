7d5f32b96cc97c767520fecba679f75a5ce5ccbd
alvin <alvynirgz@gmail.com>
2022-08-26 21:23:57 +0300

    Update Console/Install

---
+++ src/Console/Install.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alvoo\Tanda\Console;
+
+use Illuminate\Console\Command;
+
+class Install extends Command
+{
+    protected $signature = "tanda:install";
+
+    protected $description = "Install Tanda package";
+
+    public function handle()
+    {
+        $this->info('Installing Tanda...');
+        $this->info('Publishing Configuration...');
+
+        $this->call('vendor:publish', [
+      '--provider' => "Alvoo\Tanda\TandaServiceProvider",
+      '--tag' => "tanda-config"
+    ]);
+
+        $this->info('Tanda installed successfully.');
+    }
+}
